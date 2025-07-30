ef0d6756508b5b7103ecaed0559f042e1f0dbdd6
EMGR35601 <mathieu.grattard-ext@francetravail.fr>
2025-07-30 19:44:43 +0200

    Init controllers

--- src/main/java/com/ynov/sprintify/controllers/SprintController.java
+++ src/main/java/com/ynov/sprintify/controllers/SprintController.java
@@ -1,13 +1,12 @@
 package com.ynov.sprintify.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/tasks")
-public class TaskController {
+@RequestMapping("/api/sprints")
+public class SprintController {
 
-    
+
 
 }
