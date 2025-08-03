16db14da20081e759f24b66494c59fd4827bc466
Nekeroo <m.grattardpro@gmail.com>
2025-08-03 12:01:25 +0200

    Fix schema.sql and data.sql TODO : Fix the remaining TEST

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,55 +1,8 @@
-create table roles
-(
-    id   bigint auto_increment
-        primary key,
-    name varchar(50) not null,
-    constraint name
-        unique (name)
-);
+-- Insert mock roles
+INSERT INTO roles (id, name)
+values (1, 'ROLE_USER'),
+       (2, 'ROLE_ADMIN');
 
-create table users
-(
-    id       bigint auto_increment
-        primary key,
-    username varchar(50)  not null,
-    password varchar(255) not null,
-    email    varchar(100) null,
-    role_id  bigint       not null,
-    constraint username
-        unique (username),
-    constraint fk_role
-        foreign key (role_id) references roles (id)
-);
-
-create table projects
-(
-    id          bigint auto_increment
-        primary key,
-    name        varchar(100) not null,
-    description text         null,
-    owner_id    bigint       not null,
-    constraint fk_project_owner
-        foreign key (owner_id) references users (id)
-);
-
-create table sprints
-(
-    id         bigint auto_increment
-        primary key,
-    name       varchar(100) not null,
-    project_id bigint       not null,
-    constraint fk_sprint_project
-        foreign key (project_id) references projects (id)
-);
-
-create table tasks
-(
-    id          bigint auto_increment
-        primary key,
-    name        varchar(100) not null,
-    description text         null,
-    sprint_id   bigint       not null,
-    constraint fk_task_sprint
-        foreign key (sprint_id) references sprints (id)
-);
-
+-- Insert mock user
+INSERT INTO users (id, username, password, email, role_id)
+values (1, 'demo', '$2a$10$.vC4G0pAwbngnAn3zVS2N.E8jtEnvQ6QgUd/vF5E0Qc7FNmjxWp76', 'demo@gmail.com', 2);
