6fd97a66738c1900786382a67c57c651d87e7712
Nekeroo <77273805+Nekeroo@users.noreply.github.com>
2025-08-11 22:48:46 +0200

    chore: update SQL file

--- src/main/resources/db/migration/V4__drop_uk.sql
+++ src/main/resources/db/migration/V4__drop_uk.sql
@@ -1,2 +1,10 @@
+-- Supprimer la contrainte de clé étrangère
+ALTER TABLE tasks DROP FOREIGN KEY FK_task_assignee;
+
+-- Supprimer la contrainte unique
+ALTER TABLE tasks DROP INDEX UK_task_assignee;
+
+-- Recréer la clé étrangère sans contrainte unique
 ALTER TABLE tasks
-    DROP INDEX UK_task_assignee;
+    ADD CONSTRAINT FK_task_assignee
+        FOREIGN KEY (assignee_id) REFERENCES users (id);
