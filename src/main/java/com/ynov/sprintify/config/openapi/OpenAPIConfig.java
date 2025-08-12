8bee45d44d4c92497e16f65b138599899858687d
EMGR35601 <mathieu.grattard-ext@francetravail.fr>
2025-08-12 16:14:45 +0200

    chore: Ajout documentation Swagger

---
+++ src/main/java/com/ynov/sprintify/config/openapi/OpenAPIConfig.java
@@ -0,0 +1,42 @@
+package com.ynov.sprintify.config.openapi;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class OpenAPIConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Sprintify API")
+                        .description("API documentation for Sprintify app - SCRUM Project Management")
+                        .version("1.0.0")
+                        .contact(new Contact()
+                                .name("Mathieu Grattard")
+                                .email("m.grattardpro@gmail.com"))
+
+                        .license(new License()
+                                .name("MIT License")
+                                .url("https://opensource.org/licenses/MIT")))
+                .servers(List.of(
+                        new Server().url("http://localhost:8080").description("Local"),
+                        new Server().url("http://sprintify.mathieugr.fr:3000").description("Production")
+                ))
+                .components(new Components()
+                        .addSecuritySchemes("bearer-jwt", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
+    }
+}
